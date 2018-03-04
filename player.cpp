aeba819ebdc7e7c29d0ba4c19109671e79c10ccb
soyabean11 <surya.math@caltech.edu>
2018-03-03 16:20:43 -0800

    Add comment to player.cpp

--- player.cpp
+++ player.cpp
@@ -6,6 +6,8 @@
  * on (BLACK or WHITE) is passed in as "side". The constructor must finish
  * within 30 seconds.
  */
+ 
+// Testing what happens if two people change stuff
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
